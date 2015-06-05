97949381ac2fd191248cb099a96092948a4d0d37
Sam Davies <sam@razeware.com>
2015-06-05 14:36:00 +0100

    Added some sample buttons, and wired one of them up

--- app/src/main/java/com/raywenderlich/rwbutton/MainActivity.java
+++ app/src/main/java/com/raywenderlich/rwbutton/MainActivity.java
@@ -4,8 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 
 public class MainActivity extends AppCompatActivity {
+
+  private Button newButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +41,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  public void addButton(android.view.View view) {
+    // Create a new button
+    newButton = new Button(this);
+    // Set some attributes
+    newButton.setText("Magic Button");
+    // Set the layout params
+    LayoutParams layoutParams = new LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+    newButton.setLayoutParams(layoutParams);
+
+    // Add the button to the view
+    LinearLayout buttonContainer = (LinearLayout)findViewById(R.id.button_container);
+    buttonContainer.addView(newButton);
+
+    // Disable the buttons
+    enableButtonAdding(false);
+  }
+
+  private void enableButtonAdding(boolean enabled) {
+    // Find the add button
+    Button addButton = (Button)findViewById(R.id.button_add_button);
+    // Set whether it's enabled
+    addButton.setEnabled(enabled);
+
+    // Find the remove button
+    Button removeButton = (Button)findViewById(R.id.button_remove_button);
+    // Set whether it's enabled
+    removeButton.setEnabled(!enabled);
   }
 }
